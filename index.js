84c1a5c3ea6c77a3ab02cac113595f0ecd6eadd3
jroth01 <justincarlroth@gmail.com>
2018-11-23 22:39:32 -0500

    set introspection to true

--- index.js
+++ index.js
@@ -49,7 +49,11 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers,
+  introspection: true,
+});
 
 server.listen({port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
